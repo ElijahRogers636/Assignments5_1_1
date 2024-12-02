bc432505cd986190f2a343280ec22945f2e4d4e0
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-02 11:14:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignments5_1_1
+{
+    internal class Program
+    {
+        // Given an integer x, return true if x is a palindrome, and false otherwise.
+        static void Main(string[] args)
+        {
+            PrintPrompt(141); //True
+            PrintPrompt(140); //False
+            PrintPrompt(30511503); //True
+            PrintPrompt(-121); //False
+            
+        }
+
+        // Mathimatically checks if an int is a plaindrome
+        static bool IsIntPalindrome(int num)
+        {
+            int temp = num;
+            int reversedNum = 0;
+            if (num < 0) return false;
+            // Reverse int until temp is 0
+            while (temp != 0)
+            {
+                // Multiply the "reverse" number by 10 (to set up the reversed num to add the next digit) and add the remainder of temp (next digit taken from the end of temp)
+                reversedNum = (reversedNum * 10) + temp % 10; 
+                temp = temp / 10; // remove the digit on the end on the integer
+            }
+
+            // Return if reversed number is equal to the original number
+            return reversedNum == num;
+        }
+
+        // Prints the prompt
+        static void PrintPrompt(int num)
+        {
+            Console.WriteLine($"Is { num } palindromic integer?");
+            Console.WriteLine(IsIntPalindrome(num));
+            Console.WriteLine();
+        }
+    }
+}
